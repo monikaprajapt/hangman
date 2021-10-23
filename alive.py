e177abb953a7e61b1278b542cc2b0463e6cc2f60
monikaprajapt <92983459+monikaprajapt@users.noreply.github.com>
2021-10-23 09:56:00 +0530

    Add files via upload

---
+++ alive.py
@@ -0,0 +1,23 @@
+# words = "navgurukul is great"
+# counter = 0
+# while counter < len(words):
+#     print(words[counter]) 
+#     counter=counter+1
+
+
+
+# sentence = "NavGurukul is an alternative to higher education reducing the barriers of current formal education system"
+# strl=sentence.split(" ")
+# print(strl)
+# print(sentence)
+# b=[]
+# c=" "
+# for i in sentence:
+#     if i==" ":
+#         b.append(c)
+#         c=" "
+#     else:
+#         c=c+i
+# if c:
+#     b.append(c)
+# print(b)
